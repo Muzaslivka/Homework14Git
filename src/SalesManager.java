157d3191a2c15e1f67e348057e351a3c39e4c046
Margarita Dmitrieva <ms.dmitrieffa@gmail.com>
2023-02-14 23:58:34 +0300

    Add min and max value

--- src/SalesManager.java
+++ src/SalesManager.java
@@ -1,12 +1,12 @@
 public class SalesManager {
 
-    protected int[] sales = {100, 200, 300};
+    protected int[] sales = {100, 200, 300, 400, 500, 300};
 
     public SalesManager() {
         this.sales = sales;
     }
 
-    public int max(int sales) {
+    public int max() {
         int max = -1;
         for (int sale : this.sales) {
             if (sale > max) {
@@ -15,4 +15,23 @@ public class SalesManager {
         }
         return max;
     }
+
+    public int min() {
+        int min = Integer.MAX_VALUE;
+        for (int sale : sales) {
+            if (min > sale) {
+                min = sale;
+            }
+        }
+        return min;
+    }
+
+    public int average() {
+        int sum = 0;
+        for (int sale : sales) {
+            sum += sale;
+        }
+        return (sum - (min() + max())) / (sales.length - 2);
+    }
+
 }
